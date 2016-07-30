d01d4f5e5ba7e8982e1826995faf1bb96d16608d
c8888 <pedal72@gmail.com>
2016-07-30 15:35:51 +0900

    changed calcError2 from acos to cos to get lower error limit make error plots for const field and all methods

--- main.cpp
+++ main.cpp
@@ -9,12 +9,9 @@ using namespace vsr::ega;
 
 int main() {
     EulerMethodRotor* emr = new EulerMethodRotor(new Afun1); //how to create new object. It has Afun1 as A(t) and EulerMethodRotor as numerical method.
-    RungeKutta4thMethodRotor* rkmr = new RungeKutta4thMethodRotor(new Afun2);
-    RodriguesFormula* rodrConst = new RodriguesFormula(new Afun1);
-    RodriguesFormula* rodrNMR = new RodriguesFormula(new Afun2);
-    AdamsMulton* AdmConst = new AdamsMulton(new Afun1);
-    AdamsMulton* AdmConst2 = new AdamsMulton(new Afun2);
-
+    RungeKutta4thMethodRotor* runge = new RungeKutta4thMethodRotor(new Afun1);
+    RodriguesFormula* rodr = new RodriguesFormula(new Afun1);
+    AdamsMulton* Adm = new AdamsMulton(new Afun1);
 
     //CALCULATE THE ROTOR AND SPIN IN TIME
 
@@ -24,6 +21,7 @@ int main() {
     Vec S0(0,0,1);
     Rot R0(1,0,0,0);
 
+    /*
     emr->calc(dt, N, S0, R0);
     emr->timing();
     emr->saveTR("TRfirst.dat"); //export to file. Filename should be known before compliation. It by far the simplest to type it before with keyboard. Overwrites th file.
@@ -38,22 +36,26 @@ int main() {
     AdmConst->timing();
     AdmConst->saveTR("TRAdamsConst.dat"); //export to file. Filename should be known before compliation. It by far the simplest to type it before with keyboard. Overwrites th file.
     AdmConst->saveTS("TSAdamsConst.dat");
+    */
 
 
-    //CALCULATE ERROR WITH ERROR MEASUREMENT METHOD 1 (change in modulus of spin)
+    //CALCULATE ERROR WITH ERROR MEASUREMENT
     double dtmin = 1E-22;
     double dtmax = 1E-8;
     double logarithm_base = 1.1; //increment od dt for next step, i.e. dt *= logarithm_base
 
-    emr->calcError1(dtmin, dtmax, N, logarithm_base, S0, R0);
-    emr->saveError("Error1Euler.dat");
+    emr->calcError2(dtmin, dtmax, N, logarithm_base, S0, R0);
+    emr->saveError("Error2eulerConst.dat");
 
-    rodrConst->calcError1(dtmin, dtmax, N, logarithm_base, S0, R0);
-    rodrConst->saveError("rodrConstError.dat");
+    runge->calcError2(dtmin, dtmax, N, logarithm_base, S0, R0);
+    runge->saveError("Error2rungeConst.dat");
 
-    AdmConst->calcError1(dtmin, dtmax, N, logarithm_base, S0, R0);
-    AdmConst->saveError("Error1AdamsConst.dat");
+    rodr->calcError2(dtmin, dtmax, N, logarithm_base, S0, R0);
+    rodr->saveError("Error2rodrConst.dat");
 
+    Adm->calcError2(dtmin, dtmax, N, logarithm_base, S0, R0);
+    Adm->saveError("Error2adamsConst.dat");
+    
 
     return 0;
 }
